80947502a5d06e01ebd1b0deccb09e9c3b0ee35a
Zach Cullen <zacullen@gmail.com>
2020-09-14 18:19:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,15 @@ function insertNodeAt(index, newNode, linkedList, collection) {
 }
 
 function deleteNodeAt(index, linkedList, collection) {
-  // 
+  // if index is 0 set linkedlist to address at 1
+  if (index === 0) {
+    linkedList = addressAt(1, linkedList, collection)
+  } else {
+    // get address of previous node
+    let prevNode = addressAt(index - 1)
+    // set next of previous node to next of node at index (skipping over node)
+    collection[prevNode]['next'] = nodeAt(index, linkedList, collection).next
+  }
 }
 
 
